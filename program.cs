f9e00885ba8a749cf2a75da5c1233b211d87b29e
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-03-17 21:04:22 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,11 @@
+namespace LogicalProgrammingPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Main Program!!!");
+        }
+
+    }
+}
